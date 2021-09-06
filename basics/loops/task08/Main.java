e16cda3de94ebd09120180a9e1771854aa8af2cf
Vadim Vainshtein <vadim.vainshtein@gmail.com>
2021-09-06 15:00:32 +0300

    task 8 in basics/loops solved

--- basics/loops/task08/Main.java
+++ basics/loops/task08/Main.java
@@ -6,9 +6,7 @@
   * Использование: basics.loops.task08.Main m n
   * m, n - целые
   */
-//  package basics.loops.task08;
-  
-  import java.util.ArrayList;
+  package basics.loops.task08;
   
   public class Main {
   	
@@ -30,22 +28,45 @@
   			System.out.print("\nInvalid input, using a default value\n");
   		}
   		
-  		ArrayList<Byte> mDigits = new ArrayList();
-  		ArrayList<Byte> nDigits = new ArrayList();
+  		System.out.print("m = " + m + "; n = " + n + "\n\n");
+		
+		// Для определения цифр знак не имеет значения, так что числа для удобства берём по модулю
+		if(m < 0) {
+			m = -m;
+		}
+		
+		if(n < 0) {
+			n = -n;
+		}
   		
+  		//массивы для контроля появления цифр 0-9 в числах m и n
+  		boolean[] mDigits = new boolean[10];
+		boolean[] nDigits = new boolean[10];
+  		
+		//на каждом шаге делим число на 10, получая в виде остатка от деления цифру
+		// и отмечая эти цифры в соответствующем массиве
   		int divided = m;
  		do {
-  			mDigits.add((byte)(divided % 10));
+  			mDigits[divided % 10] = true;
   			divided /= 10;
   		} while(divided > 0);
   		
   		divided = n;
  		do {
-  			nDigits.add((byte)(divided % 10));
+			nDigits[divided % 10] = true;
   			divided /= 10;
  		} while(divided > 0);
   			
-  		System.out.println(mDigits);
-  		System.out.println(nDigits);
+  		// Посмотрим, какие цифры встретились в обоих числах:
+		
+		System.out.print("Common digits are:\n");
+		
+		for(int i = 0; i < 10; i++) {
+			if(mDigits[i] && nDigits[i]) {
+				System.out.print(i + "; ");
+			}
+		}
+		
+		System.out.println("");
   	}
   }
