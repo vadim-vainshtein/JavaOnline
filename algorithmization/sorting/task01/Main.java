5f457f88153301c83f1cdbe75426aad32b2553ca
Vadim Vainshtein <vadim.vainshtein@gmail.com>
2021-09-06 15:38:09 +0300

    Task 1 in algorithmization/sorting corrected

--- algorithmization/sorting/task01/Main.java
+++ algorithmization/sorting/task01/Main.java
@@ -1,9 +1,6 @@
 /*
  * Задача 1: Даны два массива и натуральное число k. Объединить их в один массив, включив второй массив между 
  * k-м и (k+1)-м элементами первого. Дополнительный массив не использовать.
- * 
- * Дополнительный массив я всё же ввожу. Возможно, я чего-то не понимаю, но по моим представлениям, для решения этой задачи без доп. массива необходимо использовать связные списки, а не массивы, но про это в условиях задачи также ничего нет.
- * Решение задачи с помощью списка трудностей не представляет, приводить его здесь не буду.
  */
 
 /*
@@ -14,6 +11,8 @@
 
 package algorithmization.sorting.task01;
 
+// Arrays.copyOf
+import java.util.Arrays;
 // мой класс для работы с массивами
 import algorithmization.Array;
 
@@ -25,13 +24,13 @@ public class Main {
 		
 		final int DEFAULT_LENGTH = 10;
 		final double MAX_VALUE = 10.0;
-		int n = DEFAULT_LENGTH;
 		int m = DEFAULT_LENGTH;
+		int n = DEFAULT_LENGTH;
 		int k = DEFAULT_LENGTH / 2;
 		
 		try {
-			n = Integer.parseInt(args[0]);
-			m = Integer.parseInt(args[1]);
+			m = Integer.parseInt(args[0]);
+			n = Integer.parseInt(args[1]);
 			k = Integer.parseInt(args[2]);
 		}
 		catch(ArrayIndexOutOfBoundsException e) {
@@ -41,12 +40,22 @@ public class Main {
 			System.out.println("\n Invalid input, using a default array length: " + DEFAULT_LENGTH);
 		}
 		
-		if(k >= n) {
-			System.out.println("\n k is greater then array length, using a smaller value: " + (k = n / 2));
+		if(m <= 0) {
+			System.out.println("\n Invalid input, using a default array length: " + DEFAULT_LENGTH);
+			m = DEFAULT_LENGTH;
 		}
 		
-		double[] a = Array.fillDouble(n, MAX_VALUE);
-		double[] b = Array.fillDouble(m, MAX_VALUE);
+		if(n <= 0) {
+			System.out.println("\n Invalid input, using a default array length: " + DEFAULT_LENGTH);
+			n = DEFAULT_LENGTH;
+		}
+		
+		if(k >= m) {
+			System.out.println("\n k is greater then array length, using a smaller value: " + (k = m / 2));
+		}
+		
+		double[] a = Array.fillDouble(m, MAX_VALUE);
+		double[] b = Array.fillDouble(n, MAX_VALUE);
 				
 		System.out.println("\nArray a:");
 		Array.printDouble(a);
@@ -55,28 +64,12 @@ public class Main {
 		System.out.println("\nk = " + k);		
 		
 		// здесь начинается решение
-		
-		double[] result = new double[n + m];
-		
-		System.arraycopy(a, 0, result, 0, k+1);
-		System.arraycopy(b, 0, result, k + 1, b.length);
-		System.arraycopy(a, k+1, result, k + b.length + 1, a.length - k - 1);
-		
-		/*
-		 * Вариант:
-		 * int i;
-		 * for(i = 0; i <= k; i++) {
-		 * 		result[i] = a[i];
-		 * }
-		 * for(int j = 0; j < b.length; j++, i++) {
-		 * 		result[i] = b[j];
-		 * }
-		 * for(int j = k+1; j < a.length; j++, i++) {
-		 * 		result[i] = a[i];
-		 * }
-		 */
+
+		a = Arrays.copyOf(a, m + n);
+		System.arraycopy(a, k + 1, a, k + 1 + n, m - k -1);
+		System.arraycopy(b, 0, a, k + 1, n);
 		
 		System.out.println("\nNew array:");
-		Array.printDouble(result);
+		Array.printDouble(a);
 	}
 }
