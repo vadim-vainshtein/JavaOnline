06fa08c151b0e1211321e884895daebdf49c0b95
Vadim Vainshtein <vadim.vainshtein@gmail.com>
2021-09-08 22:49:57 +0300

    task 6 in decomposition compiled and debugged

--- algorithmization/decomposition/task06/Main.java
+++ algorithmization/decomposition/task06/Main.java
@@ -1,11 +1,12 @@
 /**
- * Задача 6: определить, являются ли три числа взаимно просьыми
+ * Задача 6: определить, являются ли три числа взаимно простыми
  */
  
  package algorithmization.decomposition.task06;
  
  // метод для провнюерки на взаимную простоту - Util.areCoprime()
  import algorithmization.Util;
+ import algorithmization.Array;
  
  public class Main {
  	
@@ -14,7 +15,8 @@
  		int[] array1 = {2, 5, 9};
  		int[] array2 = {3, 7, 12};
  		
- 		System.out.print("Numbers " + array1.toString());
+ 		System.out.print("Numbers ");
+		Array.printInteger(array1);
  	
  		if(Util.areCoprime(array1)) {
  			System.out.println(" are coprime");
@@ -23,7 +25,8 @@
  			System.out.println(" aren't coprime");
  		}
  		
- 		System.out.print("Numbers " + array2.toString());
+ 		System.out.print("Numbers ");
+		Array.printInteger(array2);
  	
  		if(Util.areCoprime(array2)) {
  			System.out.println(" are coprime");
