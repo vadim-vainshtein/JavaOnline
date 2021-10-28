e9a63d2577c3e654b2f23c6e818f80600b29ad7f
Vadim Vainshtein <vadim.vainshtein@gmail.com>
2021-10-28 16:57:26 +0300

    implemented sorting by word length

--- strings/regex/task01/MyParser.java
+++ strings/regex/task01/MyParser.java
@@ -5,10 +5,13 @@
 package strings.regex.task01;
 
 import java.util.Arrays;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 class MyParser {
 	
 	private final String str;
+	private final String PUNCTUATION_REGEX = "[!\\?\\.]";
 	
 	public MyParser(String input) {
 		str = input;
@@ -18,10 +21,10 @@ class MyParser {
 		/**
 		 * Sorts paragraphs in str by the number of sentences
 		 */
-		
-		Paragraph[] paragraphs = Paragraph.buildArray(str);
+		// Get an array of paragraphs, using end of line symbol as a delimiter. Use PUNCTUATION_REGEX to split paragraph into sentences
+		TextPiece[] paragraphs = TextPiece.buildArray(str, "\n", PUNCTUATION_REGEX);
 		Arrays.sort(paragraphs);
-		return Paragraph.arrayToString(paragraphs);
+		return TextPiece.arrayToString(paragraphs, "\n");
 	}
 	
 	public String sortWordsByLength() {
@@ -29,7 +32,26 @@ class MyParser {
 		 * Sorts words by length in every sentence
 		 */
 		
-		return "not implemented";
+		StringBuilder result = new StringBuilder("");
+		String[] sentences = str.split(PUNCTUATION_REGEX);
+		// We need to store the punctuation to use them again in the sorted sentences
+		Matcher punctuation = Pattern.compile(PUNCTUATION_REGEX+"\n").matcher(str);
+		
+		for(int i = 0; i < sentences.length; i++) {
+			TextPiece[] words = TextPiece.buildArray(sentences[i], "\\s+", "");
+			Arrays.sort(words);
+			punctuation.find();
+			result.append(TextPiece.arrayToString(words, " ") + str.charAt(punctuation.start()));
+		}
+		
+		// Let's insert the end of line symbols back
+		for(int i = 0; i < str.length(); i++) {
+			if(str.charAt(i) == '\n') {
+				result.replace(i, i+1, String.valueOf(str.charAt(i)));
+			}
+		}
+		
+		return result.toString();
 	}
 	
 	public String sortWordsByCharOccurrence(char c) {
--- strings/regex/task01/Task01.java
+++ strings/regex/task01/Task01.java
@@ -29,7 +29,7 @@ public class Task01 {
 	
 	private static String getInputString(String[] args) {
 		// here should be a code for getting text from a file
-		String text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean convallis feugiat feugiat. Donec laoreet, odio ut imperdiet varius, augue sapien egestas mi, eget consectetur enim massa non nunc. In accumsan vulputate neque. Nullam non nunc arcu. Mauris sit amet ullamcorper massa, et dignissim augue. Suspendisse rutrum lorem commodo dictum elementum. Etiam sit amet pulvinar nisi, ut egestas ex. Aenean vitae nulla laoreet, porttitor justo non, ultricies elit. Fusce sed ultricies libero. Maecenas posuere commodo ultricies.\nVivamus molestie dui vel sem vestibulum efficitur. Morbi sed mi a dolor tincidunt porttitor ut non lectus. Donec pulvinar nisl non ligula ornare suscipit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Mauris dapibus cursus dui.\nNullam a enim at nunc hendrerit laoreet id sit amet dolor. Aliquam erat volutpat. Curabitur ac tellus auctor, sagittis est in, finibus ex. Nunc egestas tortor et augue efficitur fringilla at in nulla. Vestibulum suscipit malesuada leo ac bibendum. Duis accumsan arcu vel tortor tempor mattis. Praesent congue a leo et aliquam. Praesent at turpis scelerisque, pellentesque nunc eu, rutrum augue. Donec ut.";
+		String text = "Paragraph number one. It has two sentences.\nThe second paragraph with just one sentence.\nAnd here we have something new! Some other punctuation?";
 	
 		return text;
 	}
