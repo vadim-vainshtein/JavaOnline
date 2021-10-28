0c6be1d2ca6655b9262240cb7b8b83bf73d7e21f
Vadim Vainshtein <vadim.vainshtein@gmail.com>
2021-10-28 16:57:57 +0300

    implemented sorting by word length

--- strings/regex/task01/TextPiece.java
+++ strings/regex/task01/TextPiece.java
@@ -8,14 +8,14 @@ class TextPiece implements Comparable {
 	
 	private final String text;
 	private int numberOfUnits;
-	private final String DELIMETER;
-	private final String UNIT_DELIMETER;
+	private final String DELIMITER;
+	private final String UNIT_DELIMITER;
 	
-	public TextPiece(String input, String DELIMETER, String UNIT_DELIMETER) {
+	public TextPiece(String input, String DELIMITER, String UNIT_DELIMITER) {
 		text = input;
-		this.DELIMETER = DELIMETER;
-		this.UNIT_DELIMETER = UNIT_DELIMETER;
-		numberOfUnits = text.split(UNIT_DELIMETER).length;
+		this.DELIMITER = DELIMITER;
+		this.UNIT_DELIMITER = UNIT_DELIMITER;
+		numberOfUnits = text.split(UNIT_DELIMITER).length;
 	}
 	
 	public String toString() {
@@ -27,25 +27,25 @@ class TextPiece implements Comparable {
 		return numberOfUnits - other.numberOfUnits;
 	}
 	
-	public static TextPiece[] buildArray(String str, String DELIMETER, String UNIT_DELIMETER) {
+	public static TextPiece[] buildArray(String str, String DELIMITER, String UNIT_DELIMITER) {
 		
-		String[] strings = str.split(DELIMETER);
+		String[] strings = str.split(DELIMITER);
 		TextPiece[] texts = new TextPiece[strings.length];
 		
 		for(int i = 0; i < strings.length; i++) {
-			texts[i] = new TextPiece(strings[i], DELIMETER, UNIT_DELIMETER);
+			texts[i] = new TextPiece(strings[i], DELIMITER, UNIT_DELIMITER);
 		}
 		
 		return texts;
 	}
 	
-	public static String arrayToString(TextPiece[] texts, String DELIMETER) {
+	public static String arrayToString(TextPiece[] texts, String DELIMITER) {
 		
 		StringBuilder str = new StringBuilder("");
 		
 		int i;
 		for(i = 0; i < texts.length - 1; i++) {
-			str.append(texts[i].toString() + DELIMETER);
+			str.append(texts[i].toString() + DELIMITER);
 		}
 		
 		str.append(texts[i].toString());
